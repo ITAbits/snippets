e02c0fd191a2d431bee3f812a76c655d9820f5aa
Victor Breder <dariusskipp@gmail.com>
2017-07-27 10:09:35 -0300

    file properties doc

--- client_request/script.js
+++ client_request/script.js
@@ -2,7 +2,12 @@ const fileInput = document.getElementById("fileInput");
 const btnSend = document.getElementById("btnSend");
 
 btnSend.addEventListener("click", (e) => {
-    upload("http://localhost:8000/upload", { abacaxi: "banana", file: fileInput.files[0] }, (data) => {
+    // file: { name, size, type }
+    // file.name - file name with extension
+    // file.type - mime type
+    // file.size - size in bytes
+    let file = fileInput.files[0];
+    upload("http://localhost:8000/upload", { abacaxi: "banana", file: file }, (data) => {
         console.log(data);
     });
 });

3027f1866ef7b136ceaa055416a8b62b2328ba7b
Victor Breder <vbbreder@gmail.com>
2017-12-03 11:20:20 -0200

    yes to apt-get prompts

--- aws_ec2_instance/update.sh
+++ aws_ec2_instance/update.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
-sudo apt-get autoremove
-sudo apt-get autoclean
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
